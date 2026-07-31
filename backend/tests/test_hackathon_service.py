97079f02793e5247c0276f5b42b149f7c59383f9
Patryk Nisgorski <patryknisgorski@gmail.com>
2026-07-31 17:28:33 +0200

    Test/hackathon service cache (#39)
    * chore: fix postgres healthcheck database
    
    * test: add HackathonService cache tests
    
    * fix: add required pytest-mock to dependencies

--- backend/tests/test_hackathon_service.py
+++ backend/tests/test_hackathon_service.py
@@ -1,3 +1,6 @@
+import json
+from types import SimpleNamespace
+
 import pytest
 from redis.asyncio import from_url
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
@@ -6,7 +9,7 @@ from app.cache import REDIS_URL
 from app.db import DATABASE_URL
 from app.models import Base, Hackathon
 from app.repositories.hackathon_repository import HackathonRepository
-from app.services.hackathon_service import CACHE_KEY, HackathonService
+from app.services.hackathon_service import CACHE_KEY, CACHE_TTL_SECONDS, HackathonService
 
 
 @pytest.fixture
@@ -34,6 +37,12 @@ async def cache():
     await client.aclose()
 
 
+@pytest.fixture
+def hackathons_list():
+    hackathons_data = [{"id": 1, "name": "Python Hackathon"}, {"id": 2, "name": "AI Hackathon"}]
+    return hackathons_data
+
+
 async def test_repository_lists_hackathons(session):
     repo = HackathonRepository(session)
     hackathons = await repo.list_all()
@@ -50,3 +59,47 @@ async def test_service_caches_result(session, cache):
 
     cached_raw = await cache.get(CACHE_KEY)
     assert cached_raw is not None
+
+
+async def test_list_hackathons_returns_data_from_cache(mocker, hackathons_list):
+    cached_data = hackathons_list
+
+    repository = mocker.Mock()
+    cache = mocker.Mock()
+
+    cache.get = mocker.AsyncMock()
+    cache.set = mocker.AsyncMock()
+
+    cache.get.return_value = json.dumps(cached_data)
+
+    service = HackathonService(repository=repository, cache=cache)
+
+    hackathons = await service.list_hackathons()
+
+    assert cached_data == hackathons
+    cache.get.assert_awaited_once_with(CACHE_KEY)
+    cache.set.assert_not_awaited()
+
+
+async def test_list_hackathons_returns_data_from_repository(mocker, hackathons_list):
+    database_hackathon = [
+        SimpleNamespace(**hackathons_list[0]),
+        SimpleNamespace(**hackathons_list[1]),
+    ]
+
+    repository = mocker.Mock()
+    cache = mocker.Mock()
+
+    cache.get = mocker.AsyncMock(return_value=None)
+    cache.set = mocker.AsyncMock()
+
+    repository.list_all = mocker.AsyncMock(return_value=database_hackathon)
+    service = HackathonService(repository=repository, cache=cache)
+    hackathons = await service.list_hackathons()
+
+    assert hackathons == hackathons_list
+
+    cache.get.assert_awaited_once_with(CACHE_KEY)
+    cache.set.assert_awaited_once_with(CACHE_KEY, json.dumps(hackathons_list), ex=CACHE_TTL_SECONDS)
+
+    repository.list_all.assert_awaited_once_with()
