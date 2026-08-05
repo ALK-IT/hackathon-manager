ca99cb7e80f9b8a09637a51ce0ab11529d33a283
Patryk Nisgorski <patryknisgorski@gmail.com>
2026-08-05 16:54:42 +0200

    Add validate jwt after starting app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import api_router
+from src.auth.config import validate_configuration
 
 app = FastAPI(title="hackathon-manager API")
 app.include_router(api_router)
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    validate_configuration()
+    yield
+
 
 app.add_middleware(
     CORSMiddleware,
